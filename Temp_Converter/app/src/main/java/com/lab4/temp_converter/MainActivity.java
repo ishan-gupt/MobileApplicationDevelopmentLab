dbef6a177883e80001ab372c7a2f110a2f0a0d2a
ISHAN GUPTA vitap <19BCE7467@vitap.ac.in>
2021-10-28 12:39:46 +0530

    partially added TempConverter

---
+++ Temp_Converter/app/src/main/java/com/lab4/temp_converter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lab4.temp_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
